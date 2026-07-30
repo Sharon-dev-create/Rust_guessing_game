4d0a363264a1881e0f2e46004549f8b834d587c1
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-07-30 16:12:25 +0100

    generating the secsecret num functionality

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,17 @@
 use std::io;
 
+// Implement the random number generators method
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+
+    //declare the secret variable
+    //rand::thread_rng func gives us the particular rndom num
+    // generator we are going to use
+    // The gen_range method takes a range
+    //  expresion as an argument & generates a random num in the range.
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
