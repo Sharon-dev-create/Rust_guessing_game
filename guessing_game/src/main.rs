f802b137f6d25ac95a8c4a6750b8b5ceb4abc992
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-07-31 06:38:09 +0100

    matched the num guessed

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::io;
 
 // Implement the random number generators method
@@ -15,19 +16,26 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");    
         
     println!("You guessed: {guess}");
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+    }
     }
 }
