d0babf6b0c69950a9938b327f26be6d0666a5764
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-07-31 07:39:11 +0100

    matched the num guessed

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     //  expresion as an argument & generates a random num in the range.
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
