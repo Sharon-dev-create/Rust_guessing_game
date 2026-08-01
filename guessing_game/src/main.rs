d24c9e7388bde8ac996d15af4771624eea5b47dd
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-08-01 05:51:05 +0100

    handling errors

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::cmp::Ordering;
-
 use std::io;
+
+use std::cmp::Ordering;
 
 use rand::Rng;
 
@@ -13,6 +13,15 @@ fn main(){
         println!("Please input your guess!");
 
         let mut guess = String::new();
+
+        io::stdin()
+           .read_line(&mut guess)
+           .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+           Ok(num) => num,
+           Err(_) => continue,
+        }
 
     }
 }
