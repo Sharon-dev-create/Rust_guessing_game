bceedc4b3c93aec64880343d641f537a399f04db
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-08-02 01:19:24 +0100

    playing with rust

---
+++ variables/target/debug/deps/variables-a8c7b59ac23cf6dd.d
@@ -0,0 +1,5 @@
+/home/topgee/projects/variables/target/debug/deps/variables-a8c7b59ac23cf6dd.d: src/main.rs
+
+/home/topgee/projects/variables/target/debug/deps/variables-a8c7b59ac23cf6dd: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/topgee/projects/variables/target/debug/variables: /home/topgee/projects/variables/src/main.rs
