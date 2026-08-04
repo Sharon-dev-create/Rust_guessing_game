5120135a2ff11a9764705861560923ecfe726ca7
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-08-04 03:31:11 +0100

    temperature converte

--- Temperature_converter/src/main.rs
+++ Temperature_converter/src/main.rs
@@ -1,4 +1,14 @@
+use std::io::{self, Write};
+
+
+fn celcius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0 / 5.0) + 32.0;
+}
+
+fn fahrenheit_to_celcius(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0;
+}
+
 fn main() {
-    let mut count = 0;
-    
+
 }
